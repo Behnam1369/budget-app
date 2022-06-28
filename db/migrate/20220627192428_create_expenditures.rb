c344372ab17b3e558825bd3188a0353a7feab84b
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-28 20:31:12 +0430

    Add a reference to group in expenditure table

--- db/migrate/20220627192428_create_expenditures.rb
+++ db/migrate/20220627192428_create_expenditures.rb
@@ -1,7 +1,8 @@
 class CreateExpenditures < ActiveRecord::Migration[7.0]
   def change
     create_table :expenditures do |t|
-      t.bigint :author_id, index: true
+      t.integer :author_id, index: true
+      t.integer :group_id, index: true
       t.string :name
       t.float :amount
 
@@ -9,5 +10,6 @@ class CreateExpenditures < ActiveRecord::Migration[7.0]
     end
 
     add_foreign_key :expenditures, :users, column: :author_id, primary_key: :id
+    add_foreign_key :expenditures, :groups, column: :group_id, primary_key: :id
   end
 end
