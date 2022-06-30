62cfd1a7b4adc605f1ed43b41e60eb910bf5e702
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-30 18:26:44 +0430

    Allow upload group icon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+(function () {
+  var file = document.querySelector('.file');
+
+  group_icon.onchange = evt => {
+    const [file] = group_icon.files
+    if (file) {
+      preview.src = URL.createObjectURL(file)
+    }
+  }
+
+  document.querySelector('.btn_file').addEventListener('click', () => {
+    file.click();
+  });
+})();
