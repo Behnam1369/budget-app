3bf9bc589efb09cc0d5f977e23b848d61a040e6a
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-30 23:15:33 +0430

    Remove unused routes and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  # resources :expenditures
-  resources :groups
+  resources :groups, only: %i[index new create]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get 'groups/:group_id/expenditures', to: 'expenditures#index'
   get 'groups/:group_id/expenditures/new', to: 'expenditures#new'
-  get 'groups/:group_id/expenditures/:id/edit', to: 'expenditures#edit'
   post 'groups/:group_id/expenditures/create', to: 'expenditures#create'
-  get 'groups/:group_id/expenditures/:id', to: 'expenditures#show'
-  delete '/groups/:group_id/expenditures/:id/delete', to: 'expenditures#delete'
 
   get 'users', to: 'users#index'
   authenticated do
